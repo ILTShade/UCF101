44f303033ad8d816e1111a77169c29d1dbc6cc23
ILTShade <sunhanbo123@163.com>
2020-05-01 22:43:51 +0800

    add dataparallel and save net

--- Trainers.py
+++ Trainers.py
@@ -15,6 +15,7 @@ from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
 import torch.optim.lr_scheduler as lr_scheduler
+from torch.nn import DataParallel
 import Datasets
 import Networks
 
@@ -36,34 +37,34 @@ class Trainer(object):
         self.test_loader = self.dataset.get_loader('test')
         # networks
         self.net = Networks.Network(pretrain_path = './zoo/spatial_pretrain.pth')
-        # device
-        self.device = torch.device('cuda:0')
     def train(self):
         '''
         train network
         '''
         # set net on gpu
-        self.net.to(self.device)
+        self._load_net_device()
         # loss and optimizer
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(self.net.parameters(), lr = 1e-3, momentum = 0.9, weight_decay = 5e-4)
-        scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode = 'max', factor = 0.1, patience = 10, verbose = True)
+        scheduler = lr_scheduler.MultiStepLR(optimizer, milestones = [200, 300], gamma = 0.1)
+        # scheduler = lr_scheduler.ReduceLROnPlateau(optimizer,mode='max',factor=0.1,patience=10,verbose=True)
         # init test
         init_accuracy = self.evaluate()
         print(f'init accuracy is {init_accuracy}')
         # epochs
-        for epoch in range(500):
+        for epoch in range(400):
             # train
             self.net.train()
+            scheduler.step()
             tqdm_loader = tqdm(self.train_loader)
             for _, _, image, label in tqdm_loader:
                 output = None
                 for i in range(len(image)):
                     if i == 0:
-                        output = self.net(image[i].to(self.device))
+                        output = self.net(image[i])
                     else:
-                        output += self.net(image[i].to(self.device))
-                label = label.to(self.device)
+                        output += self.net(image[i])
+                label = label.to(output.device)
                 # loss and backward
                 loss = criterion(output, label)
                 self.net.zero_grad()
@@ -71,21 +72,21 @@ class Trainer(object):
                 optimizer.step()
                 tqdm_loader.set_description(f'loss is {loss.item():.4f}')
             # test and scheduler
-            accuracy = self.evaluate()
-            scheduler.step(accuracy)
-            print(f'epoch {epoch:04d}: accuracy is {accuracy}')
+            if epoch % 10 == 0:
+                accuracy = self.evaluate()
+                print(f'epoch {epoch+1:04d}: accuracy is {accuracy}')
+                torch.save(self.net.module.state_dict(), f'./zoo/train_{epoch+1}_{accuracy}.pth')
     def evaluate(self):
         '''
         evaluate network
         '''
+        self._load_net_device()
         self.net.eval()
-        self.net.to(self.device)
         # record result
         video_array = list()
         output_array = list()
         label_array = list()
         for _, video_name, image, label in tqdm(self.test_loader):
-            image = image.to(self.device)
             output = self.net(image)
             # transfer to cpu
             video_array.extend(list(video_name))
@@ -114,6 +115,13 @@ class Trainer(object):
                 correct_count += 1
             total_count += 1
         return correct_count / total_count
+    def _load_net_device(self):
+        '''
+        load net to device
+        '''
+        if not isinstance(self.net, DataParallel):
+            self.net.to(torch.device('cuda:0'))
+            self.net = DataParallel(self.net, device_ids = [0, 1])
 
 if __name__ == '__main__':
     trainer = Trainer()
